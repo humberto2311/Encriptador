9f5ccc1acd638f4e40ae25d249444cb79fe60da8
Beto2529 <=>
2024-07-22 15:48:45 -0500

    first_commit

--- encriptador.js
+++ encriptador.js
@@ -1,33 +1,20 @@
-function CapturarInfo() {
+document.getElementById("texto").addEventListener("input", function () {
+  document.getElementById("output").innerText = this.value;
+});
+
+function encriptar() {
   let textoEncrip = document.getElementById("texto").value;
 
-  encryptedText = textoEncrip
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/a/g, "ai")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
-  //let tokens = textoEncrip.split(" ").map(encrypt);
-  document.getElementById(
-    "result"
-  ).innerText = `Texto encriptado: ${encryptedText}`;
+  let tokens = textoEncrip.split(" ").map(encrypt);
+  //document.getElementById( "result").innerText = `Texto encriptado: ${encryptedText}`;
 }
 
-/*function encrypt(word) {
- 
-}*/
+function encrypt(word) {
+  console.log(word);
+
+  
+}
 
 function desencriptar() {
   let desencrip = document.getElementById("texto").value;
-
-  let decryptedText = desencrip
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ai/g, "a")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
-
-  document.getElementById(
-    "result"
-  ).innerText = `Texto desencriptado: ${decryptedText}`;
 }
